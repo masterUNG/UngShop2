172d6f0e6f94059f4141fb2bb5dba32b2a87aed4
MasterUNG <phrombutr@gmail.com>
2018-06-07 10:29:24 +0700

    Authen Success

--- app/src/main/java/masterung/androidthai/in/th/ungshop/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungshop/fragment/MainFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import masterung.androidthai.in.th.ungshop.R;
+import masterung.androidthai.in.th.ungshop.utility.GetAllData;
 import masterung.androidthai.in.th.ungshop.utility.MasterAlert;
+import masterung.androidthai.in.th.ungshop.utility.MasterConstant;
 
 public class MainFragment extends Fragment{
 
@@ -49,8 +56,56 @@ public class MainFragment extends Fragment{
                             getString(R.string.message_have_space));
                 } else {
 //                    No Space
+                    try {
+
+                        MasterConstant masterConstant = new MasterConstant();
+
+                        GetAllData getAllData = new GetAllData(getActivity());
+                        getAllData.execute(masterConstant.getUrlGetAllUserString());
+
+                        String resultJSON = getAllData.get();
+                        Log.d("6JuneV1", "JSON ==> " + resultJSON);
+
+                        JSONArray jsonArray = new JSONArray(resultJSON);
+
+                        boolean b = true; //True ==> UserFalse
+                        String nameString = null;
+                        String truePasswordSting = null;
+
+                        for (int i=0; i<jsonArray.length(); i+=1) {
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            if (userString.equals(jsonObject.getString("User"))) {
+
+                                b = false;
+                                nameString = jsonObject.getString("Name");
+                                truePasswordSting = jsonObject.getString("Password");
 
 
+                            }
+
+                        }   // for
+
+//                        Check User
+                        if (b) {
+//                            User False
+                            masterAlert.normalDialog("User False",
+                                    "No " + userString + " in my Database");
+                        } else if (passwordString.equals(truePasswordSting)) {
+//                            Password True
+                            Toast.makeText(getActivity(), "Welcome " + nameString, Toast.LENGTH_SHORT).show();
+
+                        } else {
+//                            Password False
+                            masterAlert.normalDialog("Password False",
+                                    "Please Try Again Password False");
+                        }
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
 
 
--- app/src/main/java/masterung/androidthai/in/th/ungshop/utility/MasterConstant.java
+++ app/src/main/java/masterung/androidthai/in/th/ungshop/utility/MasterConstant.java
@@ -3,6 +3,12 @@ package masterung.androidthai.in.th.ungshop.utility;
 public class MasterConstant {
 
     private String urlAddUserString = "http://androidthai.in.th/tod/addDataMaster.php";
+    private String urlGetAllUserString = "http://androidthai.in.th/tod/getAllUserMaster.php";
+
+
+    public String getUrlGetAllUserString() {
+        return urlGetAllUserString;
+    }
 
     public String getUrlAddUserString() {
         return urlAddUserString;
