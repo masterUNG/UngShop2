757b38292f536c46a9cf9ae74a900430e99e2b86
MasterUNG <phrombutr@gmail.com>
2018-06-07 09:35:15 +0700

    Check Space Success

--- app/src/main/java/masterung/androidthai/in/th/ungshop/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungshop/fragment/MainFragment.java
@@ -7,9 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import masterung.androidthai.in.th.ungshop.R;
+import masterung.androidthai.in.th.ungshop.utility.MasterAlert;
 
 public class MainFragment extends Fragment{
 
@@ -20,7 +23,40 @@ public class MainFragment extends Fragment{
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
+
+
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                MasterAlert masterAlert = new MasterAlert(getActivity());
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    masterAlert.normalDialog(getString(R.string.title_have_space),
+                            getString(R.string.message_have_space));
+                } else {
+//                    No Space
+
+
+                }
+
+
+            }
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtRegister);
